19ac3218b1483562a97472786357eaad4be05b1a
Unaiza Tariq <unaiza13tariq@gmail.com>
2025-05-03 00:20:46 +0500

    second commit

---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from '@clerk/nextjs'
+
+export default function Page() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100 p-4">
+      <SignUp />
+    </div>
+  )
+}
